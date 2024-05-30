f4af457e687a08e2647d23ce89d364046b3567b8
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-05-30 15:27:11 +0600

    contact with us updated

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -3,6 +3,9 @@ import { Map, Marker } from "pigeon-maps"
 const ContactUs = () => {
     return (
         <div>
+            <div className="text-center">
+            <h3 className="text-3xl font-semibold text-blue-400">Food Corner Main Location</h3>
+            </div>
             <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-100 dark:text-gray-800">
                 <div className="flex flex-col justify-between">
                     <Map height={300} defaultCenter={[22.045495808825248, 91.942596187443]} defaultZoom={11}>
